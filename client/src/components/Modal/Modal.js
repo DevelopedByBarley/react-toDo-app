48f3fd289be700cbf88394ba84bed2900298913a
Árpád <developedbybarley@gmail.com>
2022-10-30 22:24:09 +0100

    Delete modal styles created!

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -4,8 +4,10 @@ export function Modal({ onApproved, onClosed, children }) {
   return (
     <div className="modal">
       <h4>{children}</h4>
-      <button onClick={onApproved}>approved</button>
-      <button onClick={onClosed}>Closed</button>
+      <div className='modal-btns'>
+        <button className="modal-btn approve" onClick={onApproved}>Approve</button>
+        <button className="modal-btn decline" onClick={onClosed}>Close</button>
+      </div>
     </div>
   )
 }
